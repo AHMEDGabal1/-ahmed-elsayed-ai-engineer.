e24cdd016fc1026acc0f1ce5d7e5995cf0bd151c
AI inova <ahmed.elsayed.2022@Aiu.edu.eg>
2026-03-02 20:23:07 +0200

    chore: fix basePath for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: '/-ahmed-elsayed-ai-engineer.',
+  assetPrefix: '/-ahmed-elsayed-ai-engineer./',
   reactStrictMode: true,
 };
 
